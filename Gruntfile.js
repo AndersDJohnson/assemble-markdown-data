12aa6e14edb18c8937baf12c213f7addf08608f8
Anders D. Johnson <adjohnson916@users.noreply.github.com>
2015-08-21 00:52:11 -0500

    Add mocha for tests. Add test for #3.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,47 +1,62 @@
 module.exports = function (grunt) {
 
+  require('load-grunt-tasks')(grunt);
+  grunt.loadNpmTasks('assemble');
+
   //var relatedTags = require('./lib/index')();
 
   grunt.initConfig({
     assemble: {
-      example_1: {
-        options: {
-          engine: 'handlebars',
-          layout: 'example/1/src/layouts/default.hbs',
-          plugins: [
-            './lib/*.js'
-          ]
-        },
-        files: [
-          {
-            expand: true,
-            cwd: 'example/1/src/content',
-            src: ['**/*.hbs'],
-            dest: 'example/1/build'
-          }
-        ]
-      },
+      // example_1: {
+      //   options: {
+      //     engine: 'handlebars',
+      //     layout: 'test/cases/1/src/layouts/default.hbs',
+      //     plugins: [
+      //       './lib/*.js'
+      //     ]
+      //   },
+      //   files: [
+      //     {
+      //       expand: true,
+      //       cwd: 'test/cases/1/src/content',
+      //       src: ['**/*.hbs'],
+      //       dest: 'test/cases/1/build'
+      //     }
+      //   ]
+      // },
       newline: {
         options: {
           engine: 'handlebars',
           plugins: [
             './lib/*.js'
           ],
-          data: ['example/newline/data/*']
+          data: ['test/cases/newline/data/*']
         },
         files: [
           {
             expand: true,
-            cwd: 'example/newline/src/content',
+            cwd: 'test/cases/newline/src/content',
             src: ['**/*.hbs'],
-            dest: 'example/newline/build'
+            dest: 'test/cases/newline/build'
           }
         ]
+      }
+    },
+
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          // captureFile: 'test-results.txt', // Optionally capture the reporter output to a file
+          // quiet: false, // Optionally suppress output to standard out (defaults to false)
+          // clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
+        },
+        src: ['test/**/*.js']
       }
     }
   });
 
-  grunt.loadNpmTasks('assemble');
+  grunt.registerTask('test', ['assemble', 'mochaTest']);
 
   grunt.registerTask('default', ['assemble']);
 
